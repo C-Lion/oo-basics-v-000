c8cabc73993611ed26a5ec61f16a1ed590e4eaef
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-27 15:40:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class shoe 
+#   attr_accessor :author, :page_count, :genre
+  
+#   def initialize(title)
+#     @title = title
+#   end
+  
+# attr_reader :title
+  
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+end
